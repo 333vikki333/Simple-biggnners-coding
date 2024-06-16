ae35a6f85e7778bf46dc06bdf4aafe8d87a4e90f
333vikki333 <bulanshar88@gmail.com>
2024-06-16 03:42:21 -0700

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,20 @@
+import streamlit as st
+st.title('my first app')
+#st.balloons()
+(st.write('heeelo all'))
+x=st.radio('are you working',options=['yes','no'])
+if x=='yes':
+    st.write('ok you can take our weekend batch')
+else:
+    st.write('ok you can take weekkdays batch')
+    a=st.text_input('enter your branch')
+    b=st.number_input('percentage')
+ v=st.sidebar.checkbox('test cleared')
+ if v=
+    if v='true':
+    st.write'20% off')
+
+
+
+
+
---
+++ viiki.py
@@ -0,0 +1,21 @@
+import streamlit as st
+st.title('Loan Approval')
+st.write('check if you are Eligible for the loan')
+Name=st.text_input('Name')
+Age=st.number_input('Age')
+c=st.number_input('Enter Your credit score')
+s=st.number_input('Enter Your salary')
+t=st.text_input('Enter job type')
+
+if s>=50000 and c>=500:
+    st.write ('congrulation your elegible for the loan')
+else:
+    st.write ('sorry you are not elegible for loan')
+
+st.button('check')
+
+
+
+
+
+
